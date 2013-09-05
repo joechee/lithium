dc9234d725cb899e93ecec086260887869e4545f
Joe Chee <joe.chee@gmail.com>
2013-09-05 22:10:30 +0800

    added Google Analytics

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-43647847-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 var List = function (initialData) {
 	var internalList = {};
 	var $todolist = document.querySelector('.todolist');
